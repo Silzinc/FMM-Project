3b1ae7fb215f3f1267775b13bbe3435e36535da4
Jonas BLOCH <jonas.bloch@polytechnique.edu>
2024-11-29 21:18:33 +0100

    Add a comparison test between the naive and the fmm solver

--- design/fmm_tree_test.py
+++ design/fmm_tree_test.py
@@ -1,3 +1,5 @@
+from time import time
+
 from fmm_solver_design import FMMSolver, MassSample, size
 
 
@@ -5,9 +7,46 @@ def gen_random_samples(n: int):
     return [MassSample() for _ in range(n)]
 
 
+def grad_phi(xi: float) -> float:
+    return -(xi**3) / (1 + xi * xi) ** (3 / 2)
+
+
 samples = gen_random_samples(100)
-solver = FMMSolver(size, lambda x: x, 0.1, samples, 10)
+solver = FMMSolver(size, grad_phi, 0.1, samples.copy(), 5)
+naive_solver = FMMSolver(size, grad_phi, 0.1, samples.copy(), 5)
 
 solver.make_tree()
 
-solver.tree.pretty_print()
+solver.compute_multipole_extension_barycenter()
+
+solver.compute_field_tensors()
+
+ups = 100
+print(
+    f"Parameters: {ups} updates with {len(samples)} samples.\n"
+    f"Average position starts at {solver.average_pos()}.\n"
+    f"Standard deviation starts at {solver.std_pos()}."
+)
+print()
+
+print("Use fmm solver...")
+t = time()
+for _ in range(ups):
+    solver.update()
+print(f"Took {time() - t:.3f} seconds.")
+print(f"Average position is now {solver.average_pos()}")
+print(f"Standard deviation is now {solver.std_pos()}")
+print()
+
+print("Use naive solver...")
+t = time()
+for _ in range(ups):
+    naive_solver.naive_update()
+print(f"Took {time() - t:.3f} seconds.")
+print(f"Average position is now {naive_solver.average_pos()}")
+print(f"Standard deviation is now {naive_solver.std_pos()}")
+print()
+
+print(
+    f"Standard divergence between the two predictions: {solver.pos_divergence(naive_solver)}"
+)
