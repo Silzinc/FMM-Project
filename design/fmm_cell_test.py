098fd50eba9bb9b7d5db1f4276182e9146b4fac5
Silzinc <jonasbloch854@proton.me>
2025-06-05 12:08:58 +0200

    remove design directory

--- design/fmm_cell_test.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from fmm_solver_design import FMMCell, MassSample
-
-test_sample = MassSample()
-test_cell = FMMCell([test_sample], np.array([5, 5, 5]), 5)
-
-print(test_sample.pos)
-print(test_cell.contains_sample(test_sample))
--- design/mass_sample_test.py
+++
@@ -1,7 +0,0 @@
-from fmm_solver_design import MassSample
-
-test_sample = MassSample()
-
-print(test_sample.pos)
-print(test_sample.prev_pos)
-print(test_sample.speed(1))
